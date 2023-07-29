24435383255c5fe5f4374abd390ff60b348c002c
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-07-29 17:56:55 +0530

    First Commit

---
+++ HibernateDemo/src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Course.java
@@ -0,0 +1,52 @@
+package com.in28minutes.jpa.hibernate.demo.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+public class Course {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+	
+	@CreationTimestamp
+	private LocalDateTime createdDate;
+	
+	@UpdateTimestamp
+	private LocalDateTime lastUpdatedDate;
+
+	protected Course() {
+	}
+
+	public Course(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Course[%s]", name);
+	}
+}
