e9988b04e934667f383fe554cf3ac349977ab662
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-09-08 11:00:50 +0530

    Inheritance Heirarchies with JPA & Hibernate

--- HibernateDemo/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoApplication.java
+++ HibernateDemo/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.in28minutes.jpa.hibernate.demo;
 
+import java.math.BigDecimal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,9 +9,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.in28minutes.jpa.hibernate.demo.entity.Course;
-import com.in28minutes.jpa.hibernate.demo.entity.Student;
+import com.in28minutes.jpa.hibernate.demo.entity.FullTimeEmployee;
+import com.in28minutes.jpa.hibernate.demo.entity.PartTimeEmployee;
 import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
+import com.in28minutes.jpa.hibernate.demo.repository.EmployeeRepository;
 import com.in28minutes.jpa.hibernate.demo.repository.StudentRepository;
 
 @SpringBootApplication
@@ -23,6 +26,11 @@ public class DemoApplication implements CommandLineRunner{
 	@Autowired
 	private StudentRepository studentRepository;
 
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
@@ -41,7 +49,14 @@ public class DemoApplication implements CommandLineRunner{
 		
 		
 //		studentRepository.insertHardcodedStudentAndCourse();
-		studentRepository.insertStudentAndCourse(new Student("kakashi"), new Course("Spring Cloud Microservices"));
+//		studentRepository.insertStudentAndCourse(new Student("kakashi"), new Course("Spring Cloud Microservices"));
+		
+		employeeRepository.insert(new FullTimeEmployee("Gara", new BigDecimal("10000")));
+		employeeRepository.insert(new PartTimeEmployee("Sai", new BigDecimal("50")));
+		
+		logger.info("All Employees -> {}",employeeRepository.retrieveAllEmployees());
+//		
+		
 		
 	}	
 }
