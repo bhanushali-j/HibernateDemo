04c65877be50b1a6276144a2723b5884cd8964cc
Jay Bhanushali <jbhanushali@gmail.com>
2023-09-09 13:03:17 +0530

    Added pagination

--- HibernateDemo/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTest.java
+++ HibernateDemo/src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTest.java
@@ -12,6 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.in28minutes.jpa.hibernate.demo.DemoApplication;
 import com.in28minutes.jpa.hibernate.demo.entity.Course;
@@ -36,6 +40,23 @@ public class CourseSpringDataRepositoryTest {
 		Optional<Course> course=repository.findById(2000L);
 		logger.info("Course not Present : {}",course.isPresent());
 		assertFalse(course.isPresent());
+	}
+	
+	@Test
+	public void sort() {
+		Sort sort=Sort.by(Sort.Direction.ASC,"name");
+		logger.info("Sorted Courses : {}",repository.findAll(sort));
+	}
+	
+	@Test
+	public void pagination() {
+		PageRequest pageRequest=PageRequest.of(0, 2);
+		Page<Course> firstPage=repository.findAll(pageRequest);
+		logger.info("First Page : {}",firstPage.getContent());
+		
+		Pageable secondPageable=firstPage.nextPageable();
+		Page<Course> secondPage=repository.findAll(secondPageable);
+		logger.info("Second Page : {}",secondPage.getContent());
 	}
 
 }
